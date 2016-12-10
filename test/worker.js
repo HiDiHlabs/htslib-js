6becaee170e62cadc9d55b5696bdd0262ce7e560
Jeongbin Park <pjb7687@dkfz-heidelberg.de>
2016-12-10 09:55:13 +0100

    Added code for performance test

--- test/worker.js
+++ test/worker.js
@@ -3,8 +3,11 @@ importScripts('htslib_worker.js');
 onmessage = function(e) {
     cmd = e.data[0];
     if (cmd === 0) {
+        t1 = performance.now();
         fd = hts_open(e.data[1]);
         test(fd);
         hts_close(fd);
+        t2 = performance.now();
+        postMessage([2, (t2 - t1)/1000]);
     }
 };
